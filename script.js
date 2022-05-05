367fe7506728052d9fa98b94d8d9b493867eb20c
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-05-05 17:12:36 -0300

    Teste de pull

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let buttonLoggin = document.getElementById('buttonLoggin');
-let inputName = document.getElementById('inputName');
-let inputPassword = document.getElementById('inputPassword');
-
-buttonLoggin.addEventListener("click", verificacaoLogin);
+let btnLoggin = document.getElementById('btnLoggin');
+let userEmail = document.getElementById('userEmail');
+let userPwd = document.getElementById('userPwd');
+// btnLoggin.addEventListener('click', verificacaoLogin);
 function verificacaoLogin() {
-  if (inputName.value === 'tryber@teste.com' && inputPassword.value === '123456') {
+  if (userEmail.value === 'tryber@teste.com' && userPwd.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
 }
-let variv = 0;
