3e723bf4abc590f6f3dfd0c748e554460c37aec7
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-05-10 23:23:14 -0300

    Correção de lint js/css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnLoggin = document.getElementById('btnLoggin');
 const userEmail = document.getElementById('userEmail');
 const userPwd = document.getElementById('userPwd');
+const btnSend = document.getElementById('submit-btn');
+const inptAgreed = document.getElementById('agreement');
 function verificacaoLogin() {
   if (userEmail.value === 'tryber@teste.com' && userPwd.value === '123456') {
     alert('Olá, Tryber!');
@@ -8,4 +10,14 @@ function verificacaoLogin() {
     alert('Email ou senha inválidos.');
   }
 }
-btnLoggin.addEventListener('click', verificacaoLogin);
+const verifyAgreed = () => {
+  if (inptAgreed.checked === true) {
+    btnSend.style.display = 'block';
+  }
+  btnSend.style.display = 'none';
+};
+window.onload = function () {
+  btnLoggin.addEventListener('click', verificacaoLogin);
+  inptAgreed.addEventListener('click', verifyAgreed);
+  btnSend.style.display = 'none';
+};
